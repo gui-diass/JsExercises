816f8128f7cecddda8906d337c9cd644a9d5b26f
gui-diass <guidiass03@gmail.com>
2023-01-25 17:07:54 -0300

    .

--- EstruturaSequencial/ex01.js
+++ EstruturaSequencial/ex01.js
@@ -1,3 +1,6 @@
 // Faça um Programa que mostre a mensagem "Alo mundo" na tela.
 
-console.log("Alo mundo")
+ console.log("Alo mundo")
+
+
+
--- EstruturaSequencial/ex03.js
+++ EstruturaSequencial/ex03.js
@@ -1,8 +1,10 @@
 // Faça um Programa que peça dois números e imprima a soma.
 
-const num1 = 5
-const num2 = 10
+ const num1 = 5
+ const num2 = 10
 
-const soma = num1 + num2
+ const soma = num1 + num2
 
-console.log(`${num1} + ${num2} = ${soma}`)
+ console.log(`${num1} + ${num2} = ${soma}`)
+
+
